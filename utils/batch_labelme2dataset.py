3b2f4483a9bd5e94842b1c74e03b1bdc3e3e9787
zhaozhen <zhaozhen1002@yahoo.com>
2019-09-26 07:47:29 +0900

    I added utils

--- utils/batch_labelme2dataset.py
+++
@@ -1,5 +0,0 @@
-import os
-path = r'C:\Users\pasonatech\Desktop\car_copy\test/'
-json_file = os.listdir(path)
-for file in json_file:
-    os.system(r'python C:\Users\pasonatech\Anaconda3\envs\labelme\Scripts\labelme_json_to_dataset.exe %s'%(path +file))
